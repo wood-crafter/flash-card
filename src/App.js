4cbd02f6797f28500a210d27bdf91cb4cc96e63c
hungpv <phanhung2702@gmail.com>
2022-11-14 15:03:24 +0700

    [Add] login views scaffold

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import { Login } from '../src/views'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Login></Login>
     </div>
   );
 }
---
+++ src/views/login/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+export const Login = (props) => {
+  return (
+    <div className='login-container'>
+      <div className='login-form' >Login form should be here</div>
+    </div>
+  )
+}
