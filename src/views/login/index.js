1729b19270e12f0d742d31517566093cfa5ead4c
hungpv <phanhung2702@gmail.com>
2022-11-14 16:56:25 +0700

    [Add] login-form draft styling

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -3,7 +3,10 @@ import './index.css'
 export const Login = (props) => {
   return (
     <div className='login-container'>
-      <div className='login-form' >Login form should be here</div>
+      <div className='login-form' >
+        <div className='form-head'></div>
+        <div className='form-content'></div>
+      </div>
     </div>
   )
 }
