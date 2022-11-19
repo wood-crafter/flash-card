74cc470fb36debcc98d6ffb3be5736a74af5fc60
wood-crafter <phanhung2702@gmail.com>
2022-11-19 22:20:10 +0700

    [Add] use-auth provider

---
+++ src/providers/use-auth.js
@@ -0,0 +1,59 @@
+import { createContext, useCallback, useContext, useState } from 'react'
+// import { postData } from '../utils/fetch-request'
+// import { loginURL, signupURL } from '../utils/request-url'
+
+const USER_SS_KEY = '#_user_#'
+
+export const AuthContext = createContext(null)
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem(USER_SS_KEY)))
+
+  const proxiedSetUser = useCallback((user) => {
+    if (user) {
+      sessionStorage.setItem(USER_SS_KEY, JSON.stringify(user))
+    } else {
+      sessionStorage.getItem(USER_SS_KEY)
+    }
+
+    setUser(user)
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, setUser: proxiedSetUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+// export const useAuth = () => {
+//   const { user, setUser } = useContext(AuthContext)
+
+//   return {
+//     user,
+//     async attemptLogin (username, password) {
+//       const { statusCode } = await postData(loginURL, { username, password })
+
+//       if (statusCode === 200) {
+//         setUser({ username })
+//       } else {
+//         throw new Error('Credentials mismatch...!')
+//       }
+//     },
+//     async attemptSignup (username, password) {
+//       const { statusCode, errors } = await postData(signupURL, { username, password })
+
+//       if (statusCode === 200) {
+//         setUser({ username })
+//         return
+//       }
+//       if (statusCode === 409) {
+//         throw new Error('Signup infomation existed')
+//       } else {
+//         throw new Error(errors)
+//       }
+//     },
+//     async logout () {
+//       setUser(null)
+//     }
+//   }
+// }
